75e0ba16898e4b0b1b26daa36d8a422c6841b2b5
Bharti Jayprakash <bhartijayprakash19@gmail.com>
2025-04-17 12:56:25 +0530

    Created the Captain signup page

---
+++ frontend/src/pages/CaptainSignUp.jsx
@@ -0,0 +1,193 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { CaptainDataContext } from "../context/CaptainContext";
+import axios from "axios";
+
+const CaptainSignUp = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [firstname, setfirstname] = useState("");
+  const [lastname, setlastname] = useState("");
+
+  const [vehicleColor, setVehicleColor] = useState("");
+  const [vehiclePlate, setVehiclePlate] = useState("");
+  const [vehicleCapacity, setVehicleCapacity] = useState("");
+  const [vehicleType, setVehicleType] = useState("");
+
+  const { captain, setCaptain } = React.useContext(CaptainDataContext);
+
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const captainData = {
+      fullname: {
+        firstname,
+        lastname,
+      },
+      email,
+      password,
+      vehicle: {
+        color: vehicleColor,
+        plate: vehiclePlate,
+        capacity: vehicleCapacity,
+        vehicleType: vehicleType,
+      },
+    };
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/captain/register`,
+      captainData
+    );
+
+    if (response.status === 200) {
+      const data = response.data.data;
+      setCaptain(data.newCaptain);
+      localStorage.setItem("token", data.token);
+      navigate("/captain-home");
+    }
+
+    setemail("");
+    setpassword("");
+    setfirstname("");
+    setlastname("");
+    setVehicleCapacity("");
+    setVehicleColor("");
+    setVehiclePlate("");
+    setVehicleType("");
+  };
+
+  return (
+    <div className="py-5 px-5 h-screen flex flex-col justify-between ">
+      <div>
+        <img className="w-28 mb-8" src="/public/rideease-logo.png" />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">
+            What's our Captain's name
+          </h3>
+          <div className="flex gap-4 mb-6">
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2 rounded px-4 py-2 border  text-lg placeholder:text-base"
+              type="text"
+              placeholder="First name"
+              value={firstname}
+              onChange={(e) => {
+                setfirstname(e.target.value);
+              }}
+            />
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2  rounded px-4 py-2 border text-lg placeholder:text-base"
+              type="text"
+              placeholder="Last name"
+              value={lastname}
+              onChange={(e) => {
+                setlastname(e.target.value);
+              }}
+            />
+          </div>
+          <h3 className="text-lg font-medium mb-2">
+            What's our Captain's email
+          </h3>
+          <input
+            required
+            className="bg-[#eeeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="email"
+            placeholder="email@example.com"
+            value={email}
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+          />
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            required
+            className="bg-[#eeeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => {
+              setpassword(e.target.value);
+            }}
+          />
+
+          <h3 className="text-lg font-medium mb-2">Vehicle Information</h3>
+          <div className="flex gap-4 mb-7">
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              type="text"
+              placeholder="Vehicle Color"
+              value={vehicleColor}
+              onChange={(e) => {
+                setVehicleColor(e.target.value);
+              }}
+            />
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              type="text"
+              placeholder="Vehicle Plate"
+              value={vehiclePlate}
+              onChange={(e) => {
+                setVehiclePlate(e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex gap-4 mb-7">
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              type="number"
+              placeholder="Vehicle Capacity"
+              value={vehicleCapacity}
+              onChange={(e) => {
+                setVehicleCapacity(e.target.value);
+              }}
+            />
+            <select
+              required
+              className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base"
+              value={vehicleType}
+              onChange={(e) => {
+                setVehicleType(e.target.value);
+              }}
+            >
+              <option value="" disabled>
+                Select Vehicle Type
+              </option>
+              <option value="car">Car</option>
+              <option value="auto">Auto</option>
+              <option value="moto">Moto</option>
+            </select>
+          </div>
+
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base">
+            Create captain account
+          </button>
+          <p className="text-center">
+            Already have a Account?{" "}
+            <Link to="/captain-login" className="text-blue-600">
+              Login here
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div>
+        <p className="text-[10px] mt-6 leading-tight">
+          By proceeding, you consent to get calls, Whatsapp or SMS messages,
+          including by automated means, from RideEase and its affiliates to the
+          number provided.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainSignUp;
