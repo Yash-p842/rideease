85f497faa54d798f3815646153de6bc82901fa4c
Bharti Jayprakash <bhartijayprakash19@gmail.com>
2025-04-17 12:42:49 +0530

    Created the context for the user as well

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react'
+
+export const UserDataContext = createContext()
+
+const UserContext = ({children}) => {
+
+    const [user, setuser] = useState({
+        email:'',
+        fullname:{
+            firstname:'',
+            lastname:''
+        }
+    })
+
+  return (
+    <div>
+        <UserDataContext.Provider value={{user, setuser}}>
+            {children}
+        </UserDataContext.Provider>
+    </div>
+  )
+}
+
+export default UserContext
